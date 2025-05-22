110e26540c425af6987372ff08ef96d7f70c35b9
Senngy <sylvainmarchal@hotmail.com>
2025-05-22 10:09:18 +0200

    ajout de ejs et module + test home page ok

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { mainRouter } from './src/routes/main.route.js';
+
+const app = express();
+const PORT = process.env.PORT || 3500;
+
+app.set('view engine', 'ejs');
+app.set('views', './src/views');
+
+// Middleware pour parser le corps des requêtes : body-parser
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.use(mainRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
